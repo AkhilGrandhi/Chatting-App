971f51637f2ffeba0d67692f7541bbd77fb68ffb
Grandhi Akhil Sai Manoj <55430217+AkhilGrandhi@users.noreply.github.com>
2021-11-13 10:08:33 +0530

    Update firebase.js
    11/12/21

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqAXIF2kETYADLrsNQj4gOT8mS-54Illo",
-  authDomain: "intern-app-d8398.firebaseapp.com",
-  databaseURL: "https://intern-app-d8398.firebaseio.com",
-  projectId: "intern-app-d8398",
-  storageBucket: "intern-app-d8398.appspot.com",
-  messagingSenderId: "609483184975",
-  appId: "1:609483184975:web:7049fa4188abfeaf5e833a",
-  measurementId: "G-5CTYW4W7QB"
+  apiKey: "AIzaSyCX05kL9VMB4Mymjor7Amn9TYdcnAKES70",
+  authDomain: "whatsapp-clone-71f18.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-71f18.firebaseio.com",
+  projectId: "whatsapp-clone-71f18",
+  storageBucket: "whatsapp-clone-71f18.appspot.com",
+  messagingSenderId: "293408981477",
+  appId: "1:293408981477:web:e87ed2be8aeec957732f0c",
+  measurementId: "G-E51T85TQZ2"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
